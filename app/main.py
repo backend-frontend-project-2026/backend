fb421a09c825e1f81caf9fde70c3cb94add6f6a5
unknown <sammardanov@gmail.com>
2026-03-03 14:06:52 +0300

    feat(app): добавил конфиг для ruff

--- app/main.py
+++ app/main.py
@@ -3,11 +3,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
+@app.get('/')
 def main_page():
-    return {"Hello": "World"}
+    return {'Hello': 'World'}
 
 
-@app.get("/items/{item_id}")
+@app.get('/items/{item_id}')
 def get_item(item_id: int):
-    return {"item_id": item_id}
+    return {'item_id': item_id}
