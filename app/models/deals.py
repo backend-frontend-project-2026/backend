286588284d873c3646261a36f9f96b42bdb67596
unknown <sammardanov@gmail.com>
2026-03-24 20:02:12 +0300

    fix(app): relationships fix

--- app/models/deals.py
+++ app/models/deals.py
@@ -6,8 +6,7 @@ from sqlmodel import Field, Relationship
 from app.models.base import BaseModel
 
 if TYPE_CHECKING:
-    from app.models.neighbourhoods import NeighbourhoodModel
-    from app.models.dorms import DormModel
+    from app.models.reactions import ReactionModel
 
 
 class DealStatus(str, Enum):
@@ -19,16 +18,13 @@ class DealStatus(str, Enum):
 class DealModel(BaseModel, table=True):
     __tablename__ = 'deals'
 
-    like_id: int = Field(foreign_key='reactions.id')
+    reaction_id: int = Field(foreign_key='reactions.id')
     neighbourhood_id: int = Field(foreign_key='neighbourhoods.id')
     dorm_id: Optional[int] = Field(foreign_key='dorms.id')
 
     title: str = Field(max_length=100)
     status: DealStatus = Field(default=DealStatus.PENDING)
 
-    budget_min: int = Field()
-    budget_max: int = Field()
-    people_amount: int = Field()
-
-    neighbourhood: Optional['NeighbourhoodModel'] = Relationship()
-    dorm: Optional['DormModel'] = Relationship()
+    budget_min: int
+    budget_max: int
+    people_amount: int
